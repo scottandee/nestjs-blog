2b15a9d0cee13cdb4ec6c35e70c2e34af59fb34d
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-04-29 06:34:46 +0100

    feat: Setup authentication with jwt

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import { AuthGuard } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-up')
+  signUp(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
+  }
+
+  @Post('login')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { SignInDto } from './dto/sign-in.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from './dto/log-in.dto';
+require("dotenv").config();
+
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signUp(loginUserDto: LoginUserDto) {
+    // Hash Password
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    loginUserDto.password = await bcrypt.hash(loginUserDto.password, salt);
+
+    // Call the user sevice sign in method
+    const user = await this.usersService.createUser(loginUserDto);
+    return user;
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.usersService.findOne(signInDto.username);
+    if (!user) {
+        throw new UnauthorizedException()
+      }
+    const owner = await bcrypt.compare(signInDto.password, user.password)
+    if (!owner) {
+      throw new UnauthorizedException()
+    }
+    const payload = { sub: user.id, username: user.username };
+
+    return { access_token: await this.jwtService.signAsync(payload) }
+  }
+}
